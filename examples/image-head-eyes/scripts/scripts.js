6062596e7dee92afd1d5a7d2eed5abfc9ec8fb11
alexeyrozhnov <Aliaksei.Razhnou@piarainc.com>
2024-03-20 11:47:08 +0300

    fixed onRuntimeInitialized call

--- examples/image-head-eyes/scripts/scripts.js
+++ examples/image-head-eyes/scripts/scripts.js
@@ -1,33 +1,40 @@
 function openCvReady() {
-    cv['onRuntimeInitialized'] = () => {
-        const utils = new Utils('errorMessage');
-        const src = cv.imread('canvasInput');
-        const msize = new cv.Size(0, 0);
-        const gray = new cv.Mat();
-        cv.cvtColor(src, gray, cv.COLOR_RGBA2GRAY, 0);
+    cv['onRuntimeInitialized'] = onRuntimeInitialized;
 
-        const faceClassifier = addClassifier( 'haarcascade_frontalface_default.xml', '../../common/cascades/haarcascade_frontalface_default.xml', cv, utils);
-        const eyeClassifier = addClassifier( 'haarcascade_eye.xml', '../../common/cascades/haarcascade_eye.xml', cv, utils);
+    // setTimeout(() => {
+    //     onRuntimeInitialized();
+    // }, 2000);
+}
 
-        setTimeout(() => {
-            const faces = new cv.RectVector();
-            faceClassifier.detectMultiScale(gray, faces, 1.1, 3, 0, msize, msize);
-            const coordinatesFaces = getCoordinates(faces);
-            console.log(coordinatesFaces);
+function onRuntimeInitialized() {
+    const utils = new Utils('errorMessage');
+    const src = cv.imread('canvasInput');
+    const msize = new cv.Size(0, 0);
+    const gray = new cv.Mat();
+    cv.cvtColor(src, gray, cv.COLOR_RGBA2GRAY, 0);
 
-            const eyes = new cv.RectVector();
-            eyeClassifier.detectMultiScale(gray, eyes);
-            const coordinatesEyes = getCoordinates(eyes);
-            console.log(coordinatesEyes);
+    const faceClassifier = addClassifier( 'haarcascade_frontalface_default.xml', '../../common/cascades/haarcascade_frontalface_default.xml', cv, utils);
+    const eyeClassifier = addClassifier( 'haarcascade_eye.xml', '../../common/cascades/haarcascade_eye.xml', cv, utils);
 
-            src.delete();
-            gray.delete();
-            faceClassifier.delete();
-            eyeClassifier.delete();
-            faces.delete();
-            eyes.delete();
-        }, 100);
-    };
+    setTimeout(() => {
+        console.log(faceClassifier);
+        const faces = new cv.RectVector();
+        faceClassifier.detectMultiScale(gray, faces, 1.1, 3, 0, msize, msize);
+        const coordinatesFaces = getCoordinates(faces);
+        console.log(coordinatesFaces);
+
+        const eyes = new cv.RectVector();
+        eyeClassifier.detectMultiScale(gray, eyes);
+        const coordinatesEyes = getCoordinates(eyes);
+        console.log(coordinatesEyes);
+
+        src.delete();
+        gray.delete();
+        faceClassifier.delete();
+        eyeClassifier.delete();
+        faces.delete();
+        eyes.delete();
+    }, 100);
 }
 
 function addClassifier(path, url, cv, utils) {
